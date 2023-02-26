8109332b7c10698cd3a8055be5c43bd70922fe7d
Your Namecd <ayorindeayodeji1@gmail.com>
2023-02-26 20:05:41 +0100

    holiday.js

--- holiday.js
+++ holiday.js
@@ -53,10 +53,10 @@ function isLeapYear(year) {
     console.log(profitableGamble(0.9, 1, 2))
     console.log(profitableGamble(0.9, 3, 2))
   
-  //   Q5 not correct 
+  //   Q5 
   
   function makesTen(a, b) {
-      return (a === 10 , b === 10 , a + b === 10)
+      return (a === 10 || b === 10 || a + b === 10)
     }
     console.log(makesTen(9, 10))
     console.log(makesTen(9, 9))
@@ -71,7 +71,7 @@ function isLeapYear(year) {
     console.log(comp("ABC", "DE"))
     console.log(comp("hello", "edabit"))
   
-  // Q7 not correct
+  // Q7 
   
   function acceptIntoMovie(age, isSupervised) {
       if (age >= 15, isSupervised === true) {
@@ -263,35 +263,100 @@ function isLeapYear(year) {
     // Q22
 
     function match(s1, s2) {
-        return s1.toLowerCase() === s2.toLowerCase();
+        return s1.toLowerCase() === s2.toLowerCase()
       }
       
-      console.log(match("hello", "hELLo")); // Output: true
-      console.log(match("motive", "emotive")); // Output: false
-      console.log(match("venom", "VENOM")); // Output: true
-      console.log(match("mask", "mAskinG")); // Output: false
+      console.log(match("hello", "hELLo"))
+      console.log(match("motive", "emotive"))
+      console.log(match("venom", "VENOM"))
+      console.log(match("mask", "mAskinG"))
       
 
     // Q23
-
+    function createID(firstName, lastName) {
+        const firstChar = firstName.charAt(0).toLowerCase();
+        const lastThreeChars = lastName.substring(0, 3).toLowerCase();
+        const capitalizedLastThreeChars = lastThreeChars.charAt(0).toUpperCase() + lastThreeChars.slice(1);
+        return firstChar + capitalizedLastThreeChars;
+      }
+      console.log(createID("mary", "lamb"))
+      console.log(createID("John", "SMITH"))
+      console.log(createID("mary", "smith"))
 
     // Q24
 
-
+    function google(word) {
+        return word.trim().replace(/\s+/g, '+')
+      }
+      console.log(google("Enjoying     "))
+      console.log(google("Zulfah  "))
     // Q25
 
+    function reverseCapitalize(str) {
+        return str.split('').reverse().join('').toUpperCase()
+      }
+      console.log(reverseCapitalize("abc"))
+      console.log(reverseCapitalize("hellothere"))
+      console.log(reverseCapitalize("input"))
 
     // Q26
-
+    function isValidNumber(num) {
+        if (isNaN(num)) {
+          return `${num} is not a number`
+        } else {
+          return `${num} is a valid number`
+        }
+      }
+      console.log(isValidNumber(11))
+      console.log(isValidNumber("19"))
+      console.log(isValidNumber("xyz"))
+      console.log(isValidNumber("17.5"))
+      console.log(isValidNumber("21F"))
 
     // Q27
 
 
+function isEmailValid(email) {
+    if (email.indexOf("@") !== -1) {
+      return true
+    } else {
+      return false
+    }
+  }
+  console.log(isEmailValid("roshbon@gmail.com"))
+  console.log(isEmailValid("zaicongmail.com"))
+
     // Q28
 
+    function truncate_string(str, num) {
+        return str.slice(0, num)
+      }
+      console.log(truncate_string("Robin Singh", 4))
+      console.log(truncate_string("Ali Akerele", 2));
+      
 
     // Q29
 
+    function repeat_string(str, num) {
+        return str.repeat(num);
+      }
+      
+      console.log(repeat_string('Hello ', 4))
+      console.log(repeat_string('Vote wisely ', 2))
 
     // Q30
-
+    function helloWorld(num) {
+        if (num % 3 === 0 && num % 5 === 0) {
+          return "Hello World"
+        } else if (num % 3 === 0) {
+          return "Hello"
+        } else if (num % 5 === 0) {
+          return "World"
+        } else {
+          return num.toString()
+        }
+      }
+      console.log (helloWorld(3))
+      console.log (helloWorld(5))
+      console.log (helloWorld(15))
+      console.log (helloWorld(7))
