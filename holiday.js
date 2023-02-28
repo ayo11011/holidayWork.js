7502c103c66bc92445748ff465d4342aee3cc328
Your Namecd <ayorindeayodeji1@gmail.com>
2023-02-28 18:15:17 +0100

    holiday.js

--- holiday.js
+++ holiday.js
@@ -74,7 +74,7 @@ function isLeapYear(year) {
   // Q7 
   
   function acceptIntoMovie(age, isSupervised) {
-      if (age >= 15, isSupervised === true) {
+      if (age >= 15 || isSupervised === true) {
         return true
       } else {
         return false
